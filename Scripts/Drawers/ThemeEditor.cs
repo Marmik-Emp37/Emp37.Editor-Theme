4d87de25586eb86ad6bbd6cdcf6ae2c70eba07dd
Marmik Emp37 <144893751+Marmik-Emp37@users.noreply.github.com>
2026-09-09 10:05:08 +0530

    Updated greyscale.scrollbar.thumb styling

--- Scripts/Drawers/ThemeEditor.cs
+++ Scripts/Drawers/ThemeEditor.cs
@@ -9,139 +9,133 @@ using UnityEngine;
 
 namespace Emp37.ET
 {
-        using static EditorGUIUtility;
+	using static EditorGUIUtility;
 
-        [CustomEditor(typeof(Theme), true)]
-        internal class ThemeEditor : Editor
-        {
-                private const string ExportDirectory = "Assets/Editor/StyleSheets/Extensions", FileExtension = ".uss";
+	[CustomEditor(typeof(Theme), true)]
+	internal class ThemeEditor : Editor
+	{
+		private const string ExportDirectory = "Assets/Editor/StyleSheets/Extensions", FileExtension = ".uss";
 
-                private const float SearchFieldHeight = 32F, ActionButtonSize = 42F;
-                private static readonly GUIContent ApplyThemeLabel = new("Apply Theme"), ExpandLabel = new("Expand"), CollapseLabel = new("Collapse");
+		private const float SearchFieldHeight = 32F, ActionButtonSize = 42F;
+		private static readonly GUIContent ApplyThemeLabel = new("Apply Theme"), ExpandLabel = new("Expand"), CollapseLabel = new("Collapse");
 
-                private string searchQuery = string.Empty;
-
-                private SerializedProperty property_StyleRuleGroups;
-
-                private Theme Target => target as Theme;
+		private string searchQuery = string.Empty;
+		private SerializedProperty property_StyleRuleGroups;
 
 
-                private void OnEnable()
-                {
-                        property_StyleRuleGroups = serializedObject.FindProperty(nameof(Theme.StyleRuleGroups));
-                }
+		private void OnEnable()
+		{
+			property_StyleRuleGroups = serializedObject.FindProperty(nameof(Theme.StyleRuleGroups));
+		}
+		public override void OnInspectorGUI()
+		{
+			#region Search Field
+			EditorGUILayout.LabelField("Search Selectors", EditorStyles.largeLabel);
+			using (new EditorGUILayout.HorizontalScope(GUILayout.Height(SearchFieldHeight)))
+			{
+				searchQuery = EditorGUILayout.TextField(searchQuery, ETStyles.largeTextField, GUILayout.Height(SearchFieldHeight)).Trim();
 
-                public override void OnInspectorGUI()
-                {
-                        #region Search Field
-                        EditorGUILayout.LabelField("Search Selectors", EditorStyles.largeLabel);
-                        using (new EditorGUILayout.HorizontalScope(GUILayout.Height(SearchFieldHeight)))
-                        {
-                                searchQuery = EditorGUILayout.TextField(searchQuery, ETStyles.largeTextField, GUILayout.Height(SearchFieldHeight)).Trim();
+				using (new EditorGUI.DisabledGroupScope(string.IsNullOrEmpty(searchQuery)))
+				using (new ETHelpers.BackgroundColorScope(Color.red))
+				{
+					if (GUILayout.Button(ETStyles.Clear, GUILayout.Width(SearchFieldHeight), GUILayout.ExpandHeight(true)))
+					{
+						ClearSearch();
+					}
+				}
+			}
+			#endregion
 
-                                using (new EditorGUI.DisabledGroupScope(string.IsNullOrEmpty(searchQuery)))
-                                using (new ETHelpers.BackgroundColorScope(Color.red))
-                                {
-                                        if (GUILayout.Button(ETStyles.Clear, GUILayout.Width(SearchFieldHeight), GUILayout.ExpandHeight(true)))
-                                        {
-                                                ClearSearch();
-                                        }
-                                }
-                        }
-                        #endregion
+			if (string.IsNullOrWhiteSpace(searchQuery))
+			{
+				serializedObject.Update();
+				DrawPropertiesExcluding(serializedObject, "m_Script");
+				serializedObject.ApplyModifiedProperties();
 
-                        if (string.IsNullOrWhiteSpace(searchQuery))
-                        {
-                                serializedObject.Update();
-                                DrawPropertiesExcluding(serializedObject, "m_Script");
-                                serializedObject.ApplyModifiedProperties();
+				EditorGUILayout.Space(10F);
+				if (GUILayout.Button(ApplyThemeLabel, GUILayout.Height(ActionButtonSize)))
+				{
+					ExportTheme();
+				}
+				using (new EditorGUILayout.HorizontalScope())
+				{
+					if (GUILayout.Button(ExpandLabel, EditorStyles.miniButtonLeft)) ExpandProperties(true);
+					if (GUILayout.Button(CollapseLabel, EditorStyles.miniButtonRight)) ExpandProperties(false);
+				}
+				return;
+			}
 
-                                EditorGUILayout.Space(10F);
-                                if (GUILayout.Button(ApplyThemeLabel, GUILayout.Height(ActionButtonSize)))
-                                {
-                                        ExportTheme();
-                                }
-                                using (new EditorGUILayout.HorizontalScope())
-                                {
-                                        if (GUILayout.Button(ExpandLabel, EditorStyles.miniButtonLeft)) ExpandProperties(true);
-                                        if (GUILayout.Button(CollapseLabel, EditorStyles.miniButtonRight)) ExpandProperties(false);
-                                }
-                                return;
-                        }
-                        #region Search Results
-                        StyleRuleGroup[] groups = Target.StyleRuleGroups;
-                        for (int g = 0; g < groups.Length; g++)
-                        {
-                                ref StyleRuleGroup group = ref groups[g];
+			#region Search Results
+			StyleRuleGroup[] groups = (target as Theme).StyleRuleGroups;
+			for (int g = 0; g < groups.Length; g++)
+			{
+				ref StyleRuleGroup group = ref groups[g];
+				StyleRule[] rules = group.StyleRules;
+				for (int r = 0; r < rules.Length; r++)
+				{
+					ref StyleRule rule = ref rules[r];
+					string[] selectors = rule.Selectors;
+					for (int s = 0; s < selectors.Length; s++)
+					{
+						string selector = selectors[s];
+						if (!selector.Contains(searchQuery, StringComparison.OrdinalIgnoreCase)) continue;
+						using (new EditorGUILayout.HorizontalScope())
+						{
+							using (new EditorGUILayout.VerticalScope())
+							{
+								EditorGUILayout.LabelField(selector, ETStyles.largeHelpBox);
+								EditorGUILayout.LabelField($"{group.Title} > [{r}] > {selector}", EditorStyles.helpBox);
+							}
+							if (GUILayout.Button(ETStyles.INFoldout, GUILayout.Width(ActionButtonSize), GUILayout.ExpandHeight(true)))
+							{
+								ExpandProperties(false);
 
-                                StyleRule[] rules = group.StyleRules;
-                                for (int r = 0; r < rules.Length; r++)
-                                {
-                                        ref StyleRule rule = ref rules[r];
+								SerializedProperty
+									groupProperty = property_StyleRuleGroups.GetArrayElementAtIndex(g),
+									ruleProperty = groupProperty.FindPropertyRelative(nameof(group.StyleRules)).GetArrayElementAtIndex(r);
 
-                                        string[] selectors = rule.Selectors;
-                                        for (int s = 0; s < selectors.Length; s++)
-                                        {
-                                                string selector = selectors[s];
-                                                if (!selector.Contains(searchQuery, StringComparison.OrdinalIgnoreCase)) continue;
+								property_StyleRuleGroups.isExpanded = groupProperty.isExpanded = ruleProperty.isExpanded = true;
 
-                                                using (new EditorGUILayout.HorizontalScope())
-                                                {
-                                                        using (new EditorGUILayout.VerticalScope())
-                                                        {
-                                                                string displayPath = $"{group.Title} > [{r}] > {selector}";
+								ClearSearch();
+								GUIUtility.ExitGUI();
+							}
+						}
+					}
+				}
+			}
+			#endregion
+		}
 
-                                                                EditorGUILayout.LabelField(selector, ETStyles.largeHelpBox);
-                                                                EditorGUILayout.LabelField(displayPath, EditorStyles.helpBox);
-                                                        }
-                                                        if (GUILayout.Button(ETStyles.INFoldout, GUILayout.Width(ActionButtonSize), GUILayout.ExpandHeight(true)))
-                                                        {
-                                                                ExpandProperties(false);
-                                                                SerializedProperty groupProperty = property_StyleRuleGroups.GetArrayElementAtIndex(g), ruleProperty = groupProperty.FindPropertyRelative(nameof(group.StyleRules)).GetArrayElementAtIndex(r);
+		private void ExportTheme()
+		{
+			Theme target = base.target as Theme;
+			Directory.CreateDirectory(ExportDirectory);
 
-                                                                property_StyleRuleGroups.isExpanded = groupProperty.isExpanded = ruleProperty.isExpanded = true;
-                                                                ClearSearch();
-                                                                GUIUtility.ExitGUI();
-                                                        }
-                                                }
-                                        }
-                                }
-                        }
-                        #endregion
-                }
+			string path = Path.Combine(ExportDirectory, target.ThemeType + FileExtension);
+			File.WriteAllText(path, target.ToString());
 
-                private void ExportTheme()
-                {
-                        Directory.CreateDirectory(ExportDirectory);
+			AssetDatabase.Refresh();
 
-                        string path = Path.Combine(ExportDirectory, Target.ThemeType + FileExtension);
-                        File.WriteAllText(path, Target.ToString());
-
-                        AssetDatabase.Refresh();
-
-                        if ((Target.ThemeType == Theme.Type.Dark) ^ isProSkin)
-                        {
-                                InternalEditorUtility.SwitchSkinAndRepaintAllViews();
-                        }
-                        else
-                        {
-                                InternalEditorUtility.RepaintAllViews();
-                                if (Target.RecompileOnApply)
-                                {
-                                        CompilationPipeline.RequestScriptCompilation();
-                                }
-                        }
-                        GUI.skin.settings.selectionColor = Target.SelectionColor;
-                }
-                private void ExpandProperties(bool expand)
-                {
-                        SerializedProperty iterator = serializedObject.GetIterator();
-                        while (iterator.NextVisible(true)) if (iterator.depth < 4) iterator.isExpanded = expand;
-                }
-                private void ClearSearch()
-                {
-                        searchQuery = string.Empty;
-                        GUIUtility.keyboardControl = default;
-                }
-        }
+			if ((target.ThemeType == Theme.Type.Dark) ^ isProSkin) InternalEditorUtility.SwitchSkinAndRepaintAllViews();
+			else
+			{
+				InternalEditorUtility.RepaintAllViews();
+				if (target.RecompileOnApply)
+				{
+					CompilationPipeline.RequestScriptCompilation();
+				}
+			}
+			GUI.skin.settings.selectionColor = target.SelectionColor;
+		}
+		private void ExpandProperties(bool expand)
+		{
+			SerializedProperty iterator = serializedObject.GetIterator();
+			while (iterator.NextVisible(true)) if (iterator.depth < 4) iterator.isExpanded = expand;
+		}
+		private void ClearSearch()
+		{
+			searchQuery = string.Empty;
+			GUIUtility.keyboardControl = default;
+		}
+	}
 }
--- Scripts/Themes/Theme.cs
+++ Scripts/Themes/Theme.cs
@@ -6,41 +6,40 @@ using UnityEngine;
 
 namespace Emp37.ET
 {
-	  public abstract class Theme : ScriptableObject
-	  {
-			public enum Type { Light, Dark }
+	public abstract class Theme : ScriptableObject
+	{
+		public enum Type { Light, Dark }
+
+		public StyleRuleGroup[] StyleRuleGroups;
+		public Color SelectionColor;
+
+		[Tooltip("Disable to preview changes immediately as they are applied.\n\n<b>Note:</b> Some changes may require a domain reload to take full effect.")]
+		public bool RecompileOnApply;
+
+		public abstract Type ThemeType { get; }
 
 
-			public StyleRuleGroup[] StyleRuleGroups;
-			public Color SelectionColor;
+		[ContextMenu("Sort/Title")]
+		private void Sort()
+		{
+			Undo.RecordObject(this, $"Sort {nameof(StyleRuleGroups)} by Title");
+			Array.Sort(StyleRuleGroups, (a, b) => a.Title.CompareTo(b.Title));
+		}
 
-			[Tooltip("Disable to preview changes immediately as they are applied.\n\n<b>Note:</b> Some changes may require a domain reload to take full effect.")]
-			public bool RecompileOnApply;
+		[ContextMenu("Log Test Messages")]
+		private void GenerateSampleLogs()
+		{
+			Debug.Log("This is a normal log.");
+			Debug.LogWarning("This is a warning log.");
+			Debug.LogError("This is an error log.");
+		}
 
-			public abstract Type ThemeType { get; }
-
-
-			[ContextMenu("Sort/Title")]
-			private void Sort()
-			{
-				  Undo.RecordObject(this, $"Sort {nameof(StyleRuleGroups)} by Title");
-				  Array.Sort(StyleRuleGroups, (a, b) => a.Title.CompareTo(b.Title));
-			}
-
-			[ContextMenu("Log Test Messages")]
-			private void GenerateSampleLogs()
-			{
-				  Debug.Log("This is a normal log.");
-				  Debug.LogWarning("This is a warning log.");
-				  Debug.LogError("This is an error log.");
-			}
-
-			/// <summary>
-			/// Generates a finalised USS code representation for this theme.
-			/// </summary>
-			public sealed override string ToString()
-			{
-				  return $"/*----------\n[ Theme: {ThemeType}-{name} ]\n----------*/\n\n{string.Join("\n\n", from @group in StyleRuleGroups let value = @group.ToString() where !string.IsNullOrEmpty(value) select value)}";
-			}
-	  }
+		/// <summary>
+		/// Generates a finalised USS code representation for this theme.
+		/// </summary>
+		public sealed override string ToString()
+		{
+			return $"/*----------\n[ Theme: {ThemeType}-{name} ]\n----------*/\n\n{string.Join("\n\n", from @group in StyleRuleGroups let value = @group.ToString() where !string.IsNullOrEmpty(value) select value)}";
+		}
+	}
 }
